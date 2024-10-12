a40bcee2653019f440dd7fc9e949f0fda2749c21
Javizq302 <166472459+Javizq302@users.noreply.github.com>
2024-10-12 09:03:11 -0400

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.querySelector('#menu-btn').onclick = () => {
+    document.querySelector('#menu-btn').classList.toggle('fa-times');
+    document.querySelector('.navbar').classList.toggle('active');
+}
+
